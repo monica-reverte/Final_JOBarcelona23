7f2d1fa410c04e939a229163229ed8d8def79f21
Angela-GM <angelagm0112@gmail.com>
2023-04-27 13:16:27 +0200

    create css

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 import "./Header.css";
-import { BsSearch } from "react-icons/bs"
+import { BsSearch } from "react-icons/bs";
 
 export const NavBar = () => {
   return (
-    <div>
-        <h1>fndskjnfdskj</h1>
-        <div>
-            <BsSearch />
-            <input></input>
-        </div>
-    </div>
-  )
-}
+    <header>
+      <h1>TeamTracker</h1>
+      <div>
+        <input type="text"></input>
+        <BsSearch />
+      </div>
+    </header>
+  );
+};
