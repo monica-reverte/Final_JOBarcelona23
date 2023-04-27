f20a2919f4db1260ca49529c0713a20516175cbc
Angela-GM <angelagm0112@gmail.com>
2023-04-27 15:50:03 +0200

    fetch

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,33 +1,28 @@
 import React, { useEffect, useState } from "react";
 
 export const Main = () => {
-
-
   const [teams, setTeams] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     getTeams();
   }, []);
 
   const getTeams = async () => {
-      const api = await fetch("http://localhost:4000")
-      console.log(api)
-      const data = await api.json();
-      setTeams(data.recipes)
-      
-    }
-    return (
+    const api = await fetch("http://localhost:4000")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+    const data = await api.json();
+    // setTeams(data.recipes);
+  };
+
+  return (
+    <div>
+      <h3>TEAMS</h3>
       <div>
-          <h3>TEAMS</h3>
-            <div>
-              {teams.map((team) => {
-                return(
-                  <div key={team.id}>
-                    
-                  </div>
-                );
-              })}
-            </div>
+        {teams.map((team) => {
+          return <div key={team.id}></div>;
+        })}
       </div>
-    )
-  }
+    </div>
+  );
+};
