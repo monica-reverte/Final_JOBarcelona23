8b22dcfc984182e51357b25d0ae9163dda40d6d8
Angela-GM <angelagm0112@gmail.com>
2023-04-27 14:51:58 +0200

    done

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,21 +7,43 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer>
-      <div>
-        <p>TeamTracker</p>
+      <div className="rrss">
+        <div>
+          <p>TeamTracker</p>
+        </div>
+        <ul className="rss-list">
+          <li>
+            <a href="www.instagram.com" target="_blank">
+              <FaInstagram />
+            </a>
+          </li>
+          <li>
+            <a href="www.facebook.com" target="_blank">
+              <FaFacebook />
+            </a>
+          </li>
+          <li>
+            <a href="www.twitter.com" target="_blank">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
       </div>
 
-      <ul>
-        <li>
-          <FaInstagram />
-        </li>
-        <li>
-          <FaFacebook />
-        </li>
-        <li>
-          <FaTwitter />
-        </li>
-      </ul>
+      <div className="privacy">
+        <small>© 2023 TeamTracker – All Rights Reserved.</small>
+        <ul>
+          <li>
+            <a href="#">PRIVACY & COOKIE POLICY</a>
+          </li>
+          <li>
+            <a href="#">ADVERTISE</a>
+          </li>
+          <li>
+            <a href="#">CONTACT</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
