ebc95caa1109e055981aacb957b64e2d2651dc91
Angela-GM <angelagm0112@gmail.com>
2023-04-27 17:24:37 +0200

    data

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const DB_URL = "mongodb://localhost:27017/hackathon";
+const DB_URL = "mongodb://10.227.11.254:27017/hackathon";
 const connect = async () => {
   return await mongoose
     .connect(DB_URL)
