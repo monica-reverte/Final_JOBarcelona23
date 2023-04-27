6635faca3d9c0fe72f879f3b143a1324adc36a86
Angela-GM <angelagm0112@gmail.com>
2023-04-27 17:22:29 +0200

    banner

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -3,14 +3,14 @@ import React, { useState, useEffect } from "react";
 export const Main = () => {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:4000/")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.teams);
-        setData(data.teams);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data.teams);
+  //       setData(data.teams);
+  //     });
+  // }, []);
 
   return (
     <div>
